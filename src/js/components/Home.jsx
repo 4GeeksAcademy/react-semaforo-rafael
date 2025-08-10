8a0eef7687e7599eac1effb3c592e1287af0d94e
Rafael Cardona <rcardonasanta@gmail.com>
2025-08-10 13:15:18 +0000

    traffic-light-with-react-rafael

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,63 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+import React, { useState } from "react";
+
+const Home = () => {
+	// UTILIZO USESTATE PARA PODER MODIFICAR EL COLOR
+	const colors = ["red", "yellow", "green"];
+	const [color, setColor] = useState("red");
+
+	const nextColor = () => {
+		// ENCUENTRO EL COLOR ACTUAL CON FOR
+		let currentIndex = -1;
+		for (let i = 0; i < colors.length; i++) {
+			if (colors[i] === color) {
+				currentIndex = i;
+				break;
+			}
+		}
+
+		// CALCULO EL SGTE COLOR
+		let nextIndex = currentIndex + 1;
+		if (nextIndex >= colors.length) {
+		nextIndex = 0; 
+		}
+
+		// CAMBIARE EL COLOR
+
+		setColor(colors[nextIndex]);
+	};
+
+	// VOY A GUARDAR LAS VARIABLES DE CADA LUZ. 
+
+	let redClass = "light red";
+	let yellowClass = "light yellow";
+	let greenClass = "light green";
+
+	// VOY A ENCENDER LA LUZ CORRECTAMENTE
+	if (color === "red") {
+		redClass += " glow";
+	}
+	if (color === "yellow") {
+		yellowClass += " glow";
+	}
+	if (color === "green") {
+		greenClass += " glow";
+	}
+
+	return (
+		<div className="trafic-container">
+			<div className="support"></div>
+			<div className="trafic-light">
+				<div className={redClass}></div>
+				<div className={yellowClass}></div>
+				<div className={greenClass}></div>
+			</div>
+
+			<button className="button" onClick={nextColor}>
+				Cambia color
+			</button>
 		</div>
 	);
 };
