4783bcfebe59113217c66e92a0913eb6378be165
Rafael Cardona <rcardonasanta@gmail.com>
2025-08-15 23:11:10 +0000

    correction-trafficLight-rafael

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -1,11 +1,8 @@
-//create your first component
-
-import React, {useState} from "react";
+//Creo mi componente
+import React, { useState } from "react";
 
 // UTILIZO USESTATE PARA PODER MODIFICAR EL COLOR
-
 const TrafficLight = () => {
-
 	const colors = ["red", "yellow", "green"];
 	const [color, setColor] = useState("red");
 
@@ -21,34 +18,38 @@ const TrafficLight = () => {
 		// CALCULO EL SGTE COLOR
 		let nextIndex = currentIndex + 1;
 		if (nextIndex >= colors.length) {
-		nextIndex = 0; 
+			nextIndex = 0;
 		}
-		// CAMBIARE EL COLOR
+		// CAMBIO EL COLOR
 		setColor(colors[nextIndex]);
 	};
-	// VOY A GUARDAR LAS VARIABLES DE CADA LUZ. 
 
+	// VARIABLES PARA CADA LUZ
 	let redClass = "light red";
 	let yellowClass = "light yellow";
 	let greenClass = "light green";
 
-	// VOY A ENCENDER LA LUZ CORRECTAMENTE
-	if (color === "red") {
-		redClass += " glow";
-	}
-	if (color === "yellow") {
-		yellowClass += " glow";
-	}
-	if (color === "green") {
-		greenClass += " glow";
-	}
+	// SOLO ALUMBRA LA LUZ SELECCIONADA
+	if (color === "red") redClass += " glow";
+	if (color === "yellow") yellowClass += " glow";
+	if (color === "green") greenClass += " glow";
+
 	return (
 		<div className="trafic-container">
 			<div className="support"></div>
 			<div className="trafic-light">
-				<div className={redClass}></div>
-				<div className={yellowClass}></div>
-				<div className={greenClass}></div>
+				<div
+					className={redClass}
+					onClick={() => setColor("red")}
+				></div>
+				<div
+					className={yellowClass}
+					onClick={() => setColor("yellow")}
+				></div>
+				<div
+					className={greenClass}
+					onClick={() => setColor("green")}
+				></div>
 			</div>
 
 			<button className="button" onClick={nextColor}>
@@ -56,6 +57,6 @@ const TrafficLight = () => {
 			</button>
 		</div>
 	);
-    }  
+};
 
-    export default TrafficLight;
+export default TrafficLight;
